91b7563f579bb948721cb0678b84506e73393aca
hanshenrik <hh.gronsleth@gmail.com>
2018-04-15 16:40:30 +0200

    Fix vh jumping issue when URL address bar on mobile shows/hides

--- src/index.js
+++ src/index.js
@@ -50,8 +50,13 @@ app.ports.initializeJS.subscribe(function () {
     map: gmap,
   });
 
+  // Open/close artist bio when clicking artist image
   $('.artist').click(function() {
     $(this).nextAll('.artist-bio').first().slideToggle('fast');
     $(this).find('.show-more-button').first().toggleClass('open');
   })
+
+  // Set the header height based on the initial window height to avoid URL address bar show/hide
+  // change the height of the header, making the whole page jump annoyingly
+  $('#header').css('height', $(window).height());
 });
