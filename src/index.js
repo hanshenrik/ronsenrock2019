38ade924bad927e64f2d9b0122d0102d5c7042f5
hanshenrik <hh.gronsleth@gmail.com>
2018-07-20 00:04:52 +0200

    Formatting

--- src/index.js
+++ src/index.js
@@ -1,72 +1,79 @@
-import './styles/main.css';
-import { Main } from './Main.elm';
-import registerServiceWorker from './registerServiceWorker';
+import "./styles/main.css";
+import { Main } from "./Main.elm";
+import registerServiceWorker from "./registerServiceWorker";
 
-var app = Main.embed(document.getElementById('root'));
+var app = Main.embed(document.getElementById("root"));
 
 registerServiceWorker();
 
 // Initilize JS stuff when Elm is ready
-app.ports.initializeJS.subscribe(function () {
-  $('#countdown').TimeCircles({
-    animation: 'smooth',
-    circle_bg_color: '#dedede',
+app.ports.initializeJS.subscribe(function() {
+  $("#countdown").TimeCircles({
+    animation: "smooth",
+    circle_bg_color: "#dedede",
     bg_width: 0.2,
     fg_width: 0.05,
     time: {
       Days: {
-        text: 'dager',
-        color: '#95ea95',
+        text: "dager",
+        color: "#95ea95",
         show: true
       },
       Hours: {
-        text: 'timer',
-        color: '#95ea95',
+        text: "timer",
+        color: "#95ea95",
         show: true
       },
       Minutes: {
-        text: 'minutter',
-        color: '#95ea95',
+        text: "minutter",
+        color: "#95ea95",
         show: true
       },
       Seconds: {
-        text: 'sekunder',
-        color: '#95ea95',
+        text: "sekunder",
+        color: "#95ea95",
         show: true
       }
     }
   });
 
   // Initialize Mapbox
-  mapboxgl.accessToken = 'pk.eyJ1IjoiaGFuc2hlbnJpayIsImEiOiJjamV3NW1lamYwanllMndwNW11azRoa2ZmIn0.yiRRHbdHlHYxMlgRO-HnHw';
+  mapboxgl.accessToken =
+    "pk.eyJ1IjoiaGFuc2hlbnJpayIsImEiOiJjamV3NW1lamYwanllMndwNW11azRoa2ZmIn0.yiRRHbdHlHYxMlgRO-HnHw";
   var center = [11.256755, 60.368553];
   var map = new mapboxgl.Map({
-    container: 'map',
-    style: 'mapbox://styles/mapbox/streets-v10?optimize=true',
+    container: "map",
+    style: "mapbox://styles/mapbox/streets-v10?optimize=true",
     center: center,
     zoom: 10
   });
 
   var directions = new MapboxDirections({
     accessToken: mapboxgl.accessToken,
-    unit: 'metric'
+    unit: "metric"
   });
 
-  map.addControl(directions, 'top-left');
-  map.addControl(new mapboxgl.NavigationControl(), 'top-left');
-  map.addControl(new mapboxgl.FullscreenControl(), 'top-left');
+  map.addControl(directions, "top-left");
+  map.addControl(new mapboxgl.NavigationControl(), "top-left");
+  map.addControl(new mapboxgl.FullscreenControl(), "top-left");
 
-  map.on('load', () => {
-    directions.setDestination('Rønsenvegen 121, 2080 Eidsvoll');
+  map.on("load", () => {
+    directions.setDestination("Rønsenvegen 121, 2080 Eidsvoll");
   });
 
   // Open/close artist bio when clicking artist image
-  $('.artist').click(function() {
-    $(this).nextAll('.artist-bio').first().slideToggle('fast');
-    $(this).find('.show-more-button').first().toggleClass('open');
-  })
+  $(".artist").click(function() {
+    $(this)
+      .nextAll(".artist-bio")
+      .first()
+      .slideToggle("fast");
+    $(this)
+      .find(".show-more-button")
+      .first()
+      .toggleClass("open");
+  });
 
   // Set the header height based on the initial window height to avoid URL address bar show/hide
   // change the height of the header, making the whole page jump annoyingly
-  $('#header').css('height', $(window).height());
+  $("#header").css("height", $(window).height());
 });
